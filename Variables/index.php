a3992cd1cac233f63671dd244afb23ad31ce55bb
Rafael di Candia <rdicandia@gmail.com>
2024-03-04 00:20:01 +0100

    ejercicio variables Rafael

---
+++ Variables/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "ejercicios variables" ?></title>
+</head>
+<body>
+    <?php 
+        $nombre="Rafael";
+        $apellido="di Candia";
+        $edad = 34;
+        echo "<H1>Hola " .$nombre. " " .$apellido."
+        </h1>
+        <h2>Tu edad: $edad<h2>";
+
+    ?>
+<div>
+
+</div>
+</body>
+</html>
+
+
