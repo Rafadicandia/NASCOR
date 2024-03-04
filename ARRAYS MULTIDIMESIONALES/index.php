f821552ea67650dfc73af8482ab4fdc3f9686bdd
Rafael di Candia <rdicandia@gmail.com>
2024-03-04 10:10:43 +0100

    ejercicio array  multidimensional rafa

--- ARRAYS MULTIDIMESIONALES/index.php
+++ ARRAYS MULTIDIMESIONALES/index.php
@@ -6,42 +6,31 @@
     <title>Document</title>
 </head>
 <body>
-
 <?php
 $contactos = array(
-    array("nombre" => "Ramon", "telefono" => "63478967", "correoelectroinco" => "ramon98@gmail.com"),
-    array("nombre" => "Gustavo", "telefono" => "634330798", "correoelectroinco" => "gus08@hotmail.com"),
-    array("nombre" => "Susana", "telefono" => "64598904", "correoelectroinco" => "ramonholasusana@mimail.com")
+    array("nombre" => "Ramon", "telefono" => "63478967", "correoelectronico" => "ramon98@gmail.com"),
+    array("nombre" => "Gustavo", "telefono" => "634330798", "correoelectronico" => "gus08@hotmail.com"),
+    array("nombre" => "Susana", "telefono" => "64598904", "correoelectronico" => "holasusana@mimail.com")
 );
 
 foreach ($contactos as $contacto) {
-    echo "<b>".$contacto["nombre"]."</b><br>"; // Corregir la comilla de cierre
+    echo "<b>".$contacto["nombre"]."</b><br>";
     foreach ($contacto as $clave => $dato) {
-        // Corregir las comillas y agregar un espacio después de "clave"
-        echo $clave . ": " . $dato . "<br>"; // Corregir las comillas y agregar un salto de línea
+        echo $clave . ": " . $dato . "<br>";
+    }
+};
+echo "<hr>";
+   
+$contactos[] = array("nombre" => "Pedro", "telefono" => "63478967", "correoelectronico" => "Pedro98@gmail.com"); // Otra forma de agregar un nuevo contacto
+$contactos[0]["telefono"] = "834530778"; // Cambié el teléfono del primer contacto
+
+foreach ($contactos as $contacto) {
+    echo "<b>".$contacto["nombre"]."</b><br>";
+    foreach ($contacto as $clave => $dato) {
+        echo $clave . ": " . $dato . "<br>";
     }
 }
-
-   $fruta['color'] = "amarillo";
-   // Añadir
-   $fruta['temporada'] = "verano"; //otro item
-   //eliminar
-   unset($fruta["sabor"]); //Unset elimina del array
-//array_diff Crea un nuevo array sin el/los elemento/s
-   $nuevo_array = array_diff($fruta, ["amarillo"]); 
-   foreach ($fruta as $x => $y) {
-       echo "$x: $y <br>"; // La forma de imprimir claves
-     }
-// recoger una clave
-     $keys = array_keys($fruta);
-     echo $keys[1]; // ”sabor”
-
-  foreach ($fruta as $x) {
-       echo "$x <br>"; // amarillo dulce...
-     }
- ?>
-
 ?>
 
-</body>
-</html>
+<body>
+</head>
