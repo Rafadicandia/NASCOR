90f46f2da4f4b9d4c14db4e2969e081b51269bf9
Rafael di Candia <rdicandia@gmail.com>
2024-03-05 10:06:23 +0100

    datos servicor y superglobales rafa

--- DATOS SERVIDOR/index.php
+++ DATOS SERVIDOR/index.php
@@ -3,10 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Datos Servidor y Super Globales</title>
 </head>
 <body>
 <?php
+    define("nombre", "Rafael");
+    function escribeNombre(){
+        echo "Hola".nombre;
+    }
+    escribeNombre();
+    echo "<hr>";
+
 
 foreach ($_SERVER as $key => $value) {
     echo "<b>".$key."</b>: ".$value."<br>";
