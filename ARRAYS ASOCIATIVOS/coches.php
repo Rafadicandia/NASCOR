22daef64fcdef9a14350904ae7101de7a120430d
Rafael di Candia <rdicandia@gmail.com>
2024-03-07 14:06:29 +0100

    ejericicio array asociativo

--- ARRAYS ASOCIATIVOS/coches.php
+++ ARRAYS ASOCIATIVOS/coches.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Array asociativo coches</title>
 </head>
 <body>
     <?php
@@ -13,8 +13,30 @@ $auto = array(
         "modelo" => "Corolla",
         "año" => 2022
 );
-echo $auto["marca"]
 
+foreach ($auto as $clave => $valor) {
+    echo "<li>$clave: $valor</li>";
+}
+echo "<hr>";
+echo ("La nueva marca es: ").$auto["marca"]="Nissan";
+echo "<hr>";
+unset($auto["año"]);
+$auto["color"] = "Verde";
+foreach ($auto as $clave => $valor) {
+    echo "<li>$clave: $valor</li>";
+}
+echo "<hr>";
+echo "<h2>Orden por valor</h2>";
+asort($auto);
+foreach ($auto as $clave => $valor) {
+    echo "<li>$clave: $valor</li>";
+}
+echo "<hr>";
+echo "<h2>Orden por clave</h2>";
+ksort($auto);
+foreach ($auto as $clave => $valor) {
+    echo "<li>$clave: $valor</li>";
+}
 ?>
 </body>
 </html>
